8c053f847a3acc890963b557573f965552e0dcef
zachwong02 <87377427+zachwong02@users.noreply.github.com>
2022-09-14 15:21:10 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
-
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-
-sudo apt-get update
-sudo apt-get full-upgrade
-sudo apt-get install apt-transport-https
-sudo apt-get install sublime-text foremost binwalk steghide exiftool sonic-visualiser
-
 mkdir ~/tools
 
 cd ~/tools
@@ -18,3 +9,14 @@ wget https://raw.githubusercontent.com/pentestmonkey/php-reverse-shell/master/ph
 wget https://github.com/carlospolop/PEASS-ng/releases/download/20220911/linpeas.sh -O linpeas.sh
 
 wget http://www.caesum.com/handbook/Stegsolve.jar -O stegsolve.jar
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
+
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+sudo apt-get update
+sudo apt-get full-upgrade
+sudo apt-get install apt-transport-https
+sudo apt-get install sublime-text foremost binwalk steghide exiftool sonic-visualiser
+
+
