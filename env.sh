e127380f3844065cdc24b6534a28ef0de4e00c84
zachwong02 <87377427+zachwong02@users.noreply.github.com>
2022-09-21 10:38:14 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -22,10 +22,10 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | 
 
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
-sudo apt-get update
-sudo apt-get full-upgrade
-sudo apt-get install apt-transport-https
-sudo apt-get install sublime-text foremost binwalk steghide exiftool sonic-visualiser nodejs npm terminator ghidra
+sudo apt update
+sudo apt full-upgrade
+sudo apt install apt-transport-https
+sudo apt install sublime-text foremost binwalk steghide exiftool sonic-visualiser nodejs npm terminator ghidra
 
 sudo npm install snyk -g
 
